9b231738363a68f67f660905607d050fd04a3471
Eric Förster <efoerster@users.noreply.github.com>
2018-06-14 19:57:22 +0200

    Create pipe stream for the language server

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,50 @@
+import { tmpdir } from 'os';
+import { join } from 'path';
 import * as vscode from 'vscode';
+import {
+    createClientPipeTransport, generateRandomPipeName,
+    LanguageClient, LanguageClientOptions, MessageTransports, ServerOptions,
+} from 'vscode-languageclient';
+
+declare var v8debug: any;
+const DEBUG = (typeof v8debug === 'object') || startedInDebugMode();
+
+function startedInDebugMode(): boolean {
+    const args: string[] = (process as any).execArgv;
+    if (args) {
+        return args.some((arg) => /^--debug=?/.test(arg)
+            || /^--debug-brk=?/.test(arg)
+            || /^--inspect=?/.test(arg)
+            || /^--inspect-brk=?/.test(arg));
+    }
+
+    return false;
+}
+
+function generatePipeName(): string {
+    if (DEBUG) {
+        return process.platform === 'win32'
+            ? '\\\\.\\pipe\\texlab'
+            : join(tmpdir(), `texlab.sock`);
+    }
+
+    return generateRandomPipeName();
+}
+
+async function createServerStream(): Promise<MessageTransports> {
+    const name = generatePipeName();
+    const transport = await createClientPipeTransport(name);
+    const streams = await transport.onConnected();
+    return Promise.resolve({ reader: streams[0], writer: streams[1] });
+}
 
 export function activate(context: vscode.ExtensionContext) {
-    const disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        vscode.window.showInformationMessage('Hello World!');
-    });
+    const serverOptions: ServerOptions = createServerStream;
 
-    context.subscriptions.push(disposable);
+    const clientOptions: LanguageClientOptions = {
+        documentSelector: ['latex', 'tex'],
+    };
+
+    const client = new LanguageClient('texlab', 'TexLab', serverOptions, clientOptions);
+    context.subscriptions.push(client.start());
 }
