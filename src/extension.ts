2979467ef2906ca10138f86da6cf66b3a63490a2
Eric Förster <efoerster@users.noreply.github.com>
2018-07-14 21:32:28 +0200

    Replace EditorConfig with Prettier

--- src/extension.ts
+++ src/extension.ts
@@ -2,49 +2,61 @@ import { tmpdir } from 'os';
 import { join } from 'path';
 import * as vscode from 'vscode';
 import {
-    createClientPipeTransport, generateRandomPipeName,
-    LanguageClient, LanguageClientOptions, MessageTransports, ServerOptions,
+  createClientPipeTransport,
+  generateRandomPipeName,
+  LanguageClient,
+  LanguageClientOptions,
+  MessageTransports,
+  ServerOptions,
 } from 'vscode-languageclient';
 
 declare var v8debug: any;
-const DEBUG = (typeof v8debug === 'object') || startedInDebugMode();
+const DEBUG = typeof v8debug === 'object' || startedInDebugMode();
 
 function startedInDebugMode(): boolean {
-    const args: string[] = (process as any).execArgv;
-    if (args) {
-        return args.some((arg) => /^--debug=?/.test(arg)
-            || /^--debug-brk=?/.test(arg)
-            || /^--inspect=?/.test(arg)
-            || /^--inspect-brk=?/.test(arg));
-    }
+  const args: string[] = (process as any).execArgv;
+  if (args) {
+    return args.some(
+      arg =>
+        /^--debug=?/.test(arg) ||
+        /^--debug-brk=?/.test(arg) ||
+        /^--inspect=?/.test(arg) ||
+        /^--inspect-brk=?/.test(arg),
+    );
+  }
 
-    return false;
+  return false;
 }
 
 function generatePipeName(): string {
-    if (DEBUG) {
-        return process.platform === 'win32'
-            ? '\\\\.\\pipe\\texlab'
-            : join(tmpdir(), `texlab.sock`);
-    }
+  if (DEBUG) {
+    return process.platform === 'win32'
+      ? '\\\\.\\pipe\\texlab'
+      : join(tmpdir(), `texlab.sock`);
+  }
 
-    return generateRandomPipeName();
+  return generateRandomPipeName();
 }
 
 async function createServerStream(): Promise<MessageTransports> {
-    const name = generatePipeName();
-    const transport = await createClientPipeTransport(name);
-    const streams = await transport.onConnected();
-    return Promise.resolve({ reader: streams[0], writer: streams[1] });
+  const name = generatePipeName();
+  const transport = await createClientPipeTransport(name);
+  const streams = await transport.onConnected();
+  return Promise.resolve({ reader: streams[0], writer: streams[1] });
 }
 
 export function activate(context: vscode.ExtensionContext) {
-    const serverOptions: ServerOptions = createServerStream;
+  const serverOptions: ServerOptions = createServerStream;
 
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: ['latex', 'tex'],
-    };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: ['latex', 'tex'],
+  };
 
-    const client = new LanguageClient('texlab', 'TexLab', serverOptions, clientOptions);
-    context.subscriptions.push(client.start());
+  const client = new LanguageClient(
+    'texlab',
+    'TexLab',
+    serverOptions,
+    clientOptions,
+  );
+  context.subscriptions.push(client.start());
 }
