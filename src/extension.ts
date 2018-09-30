9ad41afbd737b3ec06e65f1dcabd2c86e5a94f88
Eric Förster <efoerster@users.noreply.github.com>
2018-09-30 12:42:20 +0200

    Migrate language server to Node.js

--- src/extension.ts
+++ src/extension.ts
@@ -1,21 +1,26 @@
-import * as os from 'os';
 import * as path from 'path';
 import * as vscode from 'vscode';
 import {
-  createClientPipeTransport,
-  generateRandomPipeName,
   LanguageClient,
   LanguageClientOptions,
-  MessageTransports,
   ServerOptions,
-  State,
+  TransportKind,
 } from 'vscode-languageclient';
 import { BuildFeature } from './build';
-import DEBUG from './debug';
 import { ServerIcon } from './status';
 
 export async function activate(context: vscode.ExtensionContext) {
-  const serverOptions: ServerOptions = createRpcPipe;
+  const serverModule = context.asAbsolutePath(
+    path.join('node_modules', 'texlab', 'out', 'main.js'),
+  );
+  const serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: { execArgv: ['--nolazy', '--inspect=6009'] },
+    },
+  };
   const clientOptions: LanguageClientOptions = {
     documentSelector: ['latex'],
     outputChannelName: 'LaTeX',
@@ -32,23 +37,4 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(client.start(), buildFeature, icon);
   await client.onReady();
   icon.show();
-}
-
-const RPC_DEBUG_NAME = 'texlab';
-
-function generatePipeName(debugName: string): string {
-  if (DEBUG) {
-    return process.platform === 'win32'
-      ? `\\\\.\\pipe\\${debugName}`
-      : path.join(os.tmpdir(), `${debugName}.sock`);
-  }
-
-  return generateRandomPipeName();
-}
-
-async function createRpcPipe(): Promise<MessageTransports> {
-  const name = generatePipeName(RPC_DEBUG_NAME);
-  const transport = await createClientPipeTransport(name);
-  const streams = await transport.onConnected();
-  return { reader: streams[0], writer: streams[1] };
 }
