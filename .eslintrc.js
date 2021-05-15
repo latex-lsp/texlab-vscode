6134bc7472bc6f01f164e87555a500b7cdc5b9de
Patrick Förster <pfoerster@users.noreply.github.com>
2021-05-15 21:38:38 +0200

    Fix ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
   ],
   rules: {
     '@typescript-eslint/no-use-before-define': 'off',
